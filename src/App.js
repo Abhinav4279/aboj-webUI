71b789fad073fe61d1d711820bf0ab805e42c8d5
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-09-24 22:03:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [code, setCode] = useState("");
+  const [output, setOutput] = useState("");
+  const [language, setLanguage] = useState("cpp");
+
+  const handleSubmit = async () => {
+    const payload = {
+      language,
+      code,
+    };
+    try {
+      setOutput("");
+      const { data } = await axios.post("http://localhost:5000/run", payload, {timeout: 5000});
+      setOutput(data.output);
+    } catch ({ response }) {
+      if (response) {
+        const errMsg = response.data.err.stderr;
+        setOutput(errMsg);
+      } else {
+        setOutput("Some error occurred. Please check the speed and time complexity of your code.");
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Online Code Compiler</h1>
+      <div>
+        <label>Language:</label>
+        <select
+          value={language}
+          onChange={(e) => {
+            const shouldSwitch = window.confirm(
+              "Are you sure you want to change language? WARNING: Your current code will be lost."
+            );
+            if (shouldSwitch) {
+              setLanguage(e.target.value);
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="cpp">C++</option>
+          <option value="py">Python</option>
+        </select>
+      </div>
+      <br />
+      <textarea
+        rows="20"
+        cols="75"
+        value={code}
+        onChange={(e) => {
+          setCode(e.target.value);
+        }}
+      ></textarea>
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+      <p>{output}</p>
     </div>
   );
 }
